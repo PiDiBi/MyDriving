02bbeb6db991c7c98f9a5221fdc2fb1fc8c04ac9
David Bures <david@buresovi.com>
2016-05-16 15:18:25 +0200

    hockey app keys for all platforms

--- src/MobileApps/MyDriving/MyDriving.Utils/Logger.cs
+++ src/MobileApps/MyDriving/MyDriving.Utils/Logger.cs
@@ -12,14 +12,14 @@ namespace MyDriving.Utils
     public class Logger : ILogger
     {
         static ILogger _instance;
-        public static string HockeyAppiOS => "09f39eb0435c431ebe954f6faf3a1537";
-        public static string HockeyAppAndroid => "a8d04f91d07f4e5c91be4034805af61b";
-        public static string HockeyAppUWP => "5bff51e242a84d99bddbc6037071656a";
+        public static string HockeyAppiOS => "d7be62df4e434d4caabaab06a10a0ce1 ";
+        public static string HockeyAppAndroid => "84cffbdc953c4830b506a0f7f2393337";
+        public static string HockeyAppUWP => "2c244ebe15274cca9fd222afa8583f8f";
         /// <summary>
         /// You can generate a key from: www.bingmapsportal.com
         /// This enables maps to show on the Past Trips screen
         /// </summary>
-        public static string BingMapsAPIKey => "____BingMapsAPIKey____";
+        public static string BingMapsAPIKey => "kOkHyZUptqPODTVFeozL~GKcF-fyslR6uINfG8RmwqA~AqhuayS67kocLCX1G0qJkJUj-mO2mLB93tMpnKlnmJnts_r6ZEGTszdV9QuCIjYK";
 
         public static ILogger Instance => _instance ?? (_instance = ServiceLocator.Instance.Resolve<ILogger>());
 
